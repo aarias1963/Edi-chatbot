31a3daa7e284725470509b2b001233e3fa3730ad
aarias1963 <61782547+aarias1963@users.noreply.github.com>
2024-05-13 12:12:56 +0200

    Update chat_bot_app_02.py

--- chat_bot_app_02.py
+++ chat_bot_app_02.py
@@ -23,15 +23,15 @@ if "selected_model" not in st.session_state:
 
 # Define model details
 models = {
-    "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"},
-    "llama2-70b-4096": {"name": "LLaMA2-70b-chat", "tokens": 4096, "developer": "Meta"},
     "llama3-70b-8192": {"name": "LLaMA3-70b-8192", "tokens": 8192, "developer": "Meta"},
+    "llama2-70b-4096": {"name": "LLaMA2-70b-chat", "tokens": 4096, "developer": "Meta"},
     "llama3-8b-8192": {"name": "LLaMA3-8b-8192", "tokens": 8192, "developer": "Meta"},
     "mixtral-8x7b-32768": {"name": "Mixtral-8x7b-Instruct-v0.1", "tokens": 32768, "developer": "Mistral"},
+    "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"}
 }
 
 # Layout for model selection and max_tokens slider
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     model_option = st.sidebar.selectbox(
@@ -59,9 +59,6 @@ with col2:
         step=512,
         help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}"
     )
-
-with col3:
-    system_prompt = st.sidebar.text_input("Prompt del sistema:")
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
