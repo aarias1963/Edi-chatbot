6424af8fc95f74ea1a23219a7798f416ecf17836
aarias1963 <61782547+aarias1963@users.noreply.github.com>
2024-05-13 12:46:50 +0200

    Update chat_bot_app_02.py

--- chat_bot_app_02.py
+++ chat_bot_app_02.py
@@ -13,7 +13,7 @@ st.image('Logo_Edinumen_S.png', caption=None, width=80, clamp=False, channels="R
 st.subheader("Groq Chat Streamlit App", divider="rainbow", anchor=False)
 
 client = Groq(
-    groq_api_key = os.environ['GROQ_API_KEY'],
+    api_key = os.environ['GROQ_API_KEY'],
 )
 
 # Initialize chat history and selected model
